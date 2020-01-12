e57083aad08000d5e22957c3825dc58470a9e529
sussica <sussicaya@gmail.com>
2020-01-11 23:15:12 -0800

    haha

--- query.js
+++ query.js
@@ -1,102 +1,69 @@
-const {Pool} = require('pg');
+const {
+  Pool
+} = require('pg');
+const request = require('request');
 require('dotenv').config();
 const moment = require('moment');
 
+
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWRD,
-    port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWRD,
+  port: process.env.DB_PORT,
 });
 
-/* function createReserve(request, response) {
-    // assume all params are valid strings
-    const vtname = request.body.vtname;
-    const dlicense = request.body.dlicense;
-    const location = request.body.location;
-    const city = request.body.city;
-    const fromdate = request.body.fromdate;
-    const todate = request.body.todate;
-    const fromtime = request.body.fromtime;
-    const totime = request.body.totime;
-    // query: check if such vehicle is available
-    let rentResult;
-    return pool.query(`SELECT * from vehicletype WHERE vtname = $1`, [vtname]).then(result => {
-        if (result.rows.length === 0) {
-            return Promise.reject({message: "Given vehicle type does not exist."});
-        }
-        return pool.query(`SELECT * from vehicle WHERE location = $1 AND city = $2`, [location, city]);
-    })
-        .then(result => {
-            if (result.rows.length === 0) {
-                return Promise.reject({message: "Given branch does not exist."});
-            }
-            return pool.query(`SELECT * from customer WHERE dlicense = $1`, [dlicense]);
-        })
-        .then(result => {
-            if (result.rows.length === 0) {
-                return Promise.reject({message: "No customer found in database with given driver's license."});
-            }
-            return pool.query(vehicleRentQuery, [vtname, location, city, fromdate, todate, fromtime, totime]);
-        })
-        .then(result => {
-            // throws error if no reservation slot is available
-            if (result.rows.length === 0) {
-                return Promise.reject({message: "No such vehicle available for rent."});
-            }
-            rentResult = result;
-            return pool.query(vehicleReservationQuery, [vtname, location, city, fromdate, todate, fromtime, totime]);
-        })
-        .then(reserveResult => {
-            if (rentResult.rows.length <= reserveResult.rows.length) {
-                return Promise.reject({message: "All such vehicles are either reserved or rented."});
-            }
-            return pool.query(`SELECT confno FROM reservation WHERE dlicense = $1 AND confno IN
-            (SELECT confno FROM rental WHERE rid NOT IN (SELECT rid FROM return))`, [dlicense]);
-        }).then(confnoResult => {
-            if (confnoResult.rows.length > 0) {
-                return Promise.reject({message: "Only one reservation can be made per customer. You have either made a reservation with" +
-                        " the confirmation number: " + confnoResult.rows[0].confno + ". Or you're are currently renting using the given reservation number."});
-            }
-            // perform actual reservation, returning confno
-            return pool.query(`INSERT INTO reservation(vtname, dlicense, location, city, fromdate, todate, fromtime, totime)
-                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
-                [vtname, dlicense, location, city, fromdate, todate, fromtime, totime]);
-        })
-        .then(result => {
-            // data = confno
-            return response.json({
-                data: result.rows[0]
-            });
-        })
-        .catch(error => {
-            return response.send({
-                error: error,
-                message: "Problem Creating Reservation"
-            });
-        });
-} */
 
 async function send(request, response) {
-    const image = response.body.image;
-    const conversation = response.body.conversation;
-    const emotionData = await processImage(image);
-    const keyword = await processConversation(conversation);
-    // TODO store into pg
+  // const image = response.body.image;
+  // const conversation = response.body.conversation;
+   const image = 0;
+  const emotionData = await processImage(image);
+  const keyword = await processConversation(conversation);
+  return emotionData;
+  // TODO store into pg
 }
 
 async function processImage(image) {
-    // TODO
+
+  const requestURL = "https://pocket-therapist.cognitiveservices.azure.com/face/v1.0/detect";
+  const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg';
+  const subscriptionKey = 'c7c46173f02741f584c590b5e731582b';
+  const params = {
+    'returnFaceId': 'false',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'emotion',
+  };
+  const options = {
+    uri: requestURL,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}' ,
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': subscriptionKey
+    }
+  };
+
+  request.post(options, (error, response, body) => {
+    if (error) {
+      console.log('Error: ', error);
+      return;
+    }
+    let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+    console.log('JSON Response\n');
+    console.log(jsonResponse);
+  });
 }
 
 async function processConversation(sentence) {
-    const params = {
-        // Request params
-    }
+  const params = {
+    // Request params
+  }
 }
 
 module.exports = {
-    getVehicle,
-    send
+  // getVehicle,
+  send
 };
